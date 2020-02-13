7b89afd368b19626e198c0c1e2e04bdae0ddec91
Benjamin Thom and Kristina Hengster Tintor <kristina@>
2020-02-13 14:49:01 -0800

    update dino-service to include an if else statement to check for response ok and response status

--- src/dino-service.js
+++ src/dino-service.js
@@ -2,12 +2,14 @@ export class DinoService {
   async getDinoIpsum(input1, input2) {
     try {
       let response = await fetch(`http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=${input1}&words=${input2}`);
-      if (response.status != 200 || response.ok != true) {
-        return false;
+      let jsonifiedResponse;
+      if (response.ok && response.status === 200) {
+        jsonifiedResponse = await response.json();
+      } else {
+        jsonifiedResponse = false;
       }
-      let jsonifiedResponse = await response.json();
       return jsonifiedResponse;
-    } catch(error) {
+    } catch (error) {
       return false;
     }
   }
